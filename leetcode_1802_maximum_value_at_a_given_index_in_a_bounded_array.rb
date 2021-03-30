d9056ddbd68650e7eb4e1acc2fd0683cdee6d135
zkobrinsky <zachkobrinsky@gmail.com>
2021-03-30 12:16:56 -0400

    started but incomplete

---
+++ leetcode_1802_maximum_value_at_a_given_index_in_a_bounded_array.rb
@@ -0,0 +1,20 @@
+"https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/"
+# UNFINISHED
+
+n = 4
+index = 2
+max_sum = 6
+
+
+def max_value(n, index, max_sum)
+    nums = Array.new(4,1)
+    # [1,1,1,1]
+           i
+    nums[index] = n-1
+    p nums
+    # what's the highest number at nums[index] you can get where the sum of the array = maxSum and the arr length is n
+    # 
+end
+
+max_value(n, index, max_sum)
+
